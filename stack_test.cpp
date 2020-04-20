e99d74590b71aa6a57932c0f850c576b9468db3e
AndrewHuagit <57641525+AndrewHuagit@users.noreply.github.com>
2020-04-20 12:30:36 -0700

    test change

--- stack_test.cpp
+++ stack_test.cpp
@@ -16,7 +16,26 @@ void test_push_three_items() {
 
 }
 
+void test_pop_from_empty(){
+  Stack s = Stack();
+  s.pop();
+}
+
+void test_copy_stack(){
+  Stack s1 = Stack();
+  s1.push(1);
+  s1.push(2);
+  s1.push(3);
+  Stack s2 = Stack(s1);
+  s1.pop();
+  assert (s2.pop() == 3);
+  assert (s2.pop() == 2);
+  assert (s2.pop() == 1);
+}
+
 int main() {
-    test_push_three_items();
-    return 0;
+  test_push_three_items();
+  test_pop_from_empty();
+  test_copy_stack();
+  return 0;
 }
